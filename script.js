95103e128c0dafa2ef2a13e3dcf4fdb8cf4a41d2
Gabriel Dias <gabriel_dias_santiago@hotmail.com>
2022-11-04 12:51:27 -0300

    first

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var theevent = new Date("Dec 04 2022 00:00:01");
 var seconds = (theevent - now) / 1000;
 var minutes = seconds / 60;
 var hours = minutes / 60;
+var input = document.getElementById('days')
 var days = hours / 24;
 ID = window.setTimeout("update();", 1000);
+input.style.display = 'block'
 
 function update() {
   now = new Date();
@@ -17,5 +19,6 @@ function update() {
   days = hours / 24;
   days = Math.round(days);
   document.contagemregressiva.days.value = 'Em apenas ' + days + ' dias';
+
   ID = window.setTimeout("update();", 1000);
 }
